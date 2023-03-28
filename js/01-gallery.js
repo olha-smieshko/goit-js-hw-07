2cf0261915114dab87d9903b567ca26d9718b7a9
olha-smieshko <smeshkoom@gmail.com>
2023-03-28 03:09:38 +0300

    finale

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+/**** Функція для формування рядка розмітки галереї */
+function createGalleryMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+        </a>
+      </li>
+    `;
+  }).join('');
+}
+
+/**** Створюю розмітку в html - Візуалізація галереї */
+gallery.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+/**** Додаю слухач події на галерею - делегування подій кліку на зображенні зі списку на загальний елемент списку (ul)*/
+gallery.addEventListener('click', openModal);
+
+/**** Колбек функція слухача подій - відкриття модального вікна*/
+function openModal(event) {
+    /**** Скидаю дефолтну поведінку браузера - щоб посилання не відкривалось у новій вкладці*/
+    event.preventDefault();
+    /**** Дізнаюсь посилання на велике зображення картинки, на яку клікнули*/
+    const largeImage = event.target.dataset.source;
+    /**** Перевіряю, чи клік стався саме на зображенні*/
+    if (!largeImage) {
+        return;
+    }
+    /**** Створюю модальнре вікно, методами бібліотеки basicLightbox*/
+    const modal = basicLightbox.create(`<img src="${largeImage}">`);
+    /**** Відкриваю модальнре вікно, методами бібліотеки basicLightbox*/
+    modal.show();
+    /**** Функція закриття модального вікна по клавіші Escape*/
+    const handleKeyDown = (event) => {
+        if (event.code === 'Escape') {
+            modal.close();
+        }
+    };
+    /**** Додаю слухач на клавішу клавіатури*/
+    document.addEventListener('keydown', handleKeyDown);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+/**** Функція для формування рядка розмітки галереї */
+function createGalleryMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}">
+        </a>
+      </li>
+    `;
+  }).join('');
+}
+
+/**** Створюю розмітку в html - Візуалізація галереї */
+gallery.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+/**** Створюю модальнре вікно, методами бібліотеки SimpleLightbox*/
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250
+});    
